03e8af68ca2d1de8ad680a9f58c53252b59124b6
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-02-08 22:44:12 -0200

    Adding crucial configuration to use typescript instead of coffescript.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module RailsSkeleton
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #Typescript
+    TypeScript::Src.use_external_tsc = true
+    Typescript::Rails::Compiler.default_options = %w(--lib es7)
+    config.app_generators.javascript_engine :typescript
+    config.assets.enabled = true
   end
 end

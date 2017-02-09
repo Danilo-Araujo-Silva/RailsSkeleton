2963e2aef41cb1202f2b4fc42056fe254f73f750
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-02-09 15:10:49 -0200

    Merging with Angular quickstart. Creating the controller test and action index. Unorganized yet.

---
+++ app/assets/javascripts/test.ts
@@ -0,0 +1,19 @@
+async function slow() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            console.log("slow finished");
+            resolve();
+        }, 3000);
+    });
+}
+
+function fast() {
+    console.log("fast");
+}
+
+async function run() {
+    await slow();
+    fast();
+}
+
+run();

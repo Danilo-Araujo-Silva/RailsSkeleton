dddfc35a37028ae58fcc19d7c95a1ebc666d506d
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-20 16:33:54 -0300

    Generating permissions scaffold. Improving migrations.

---
+++ app/views/permissions/_permission.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! permission, :id, :parent, :breadcrumb, :title, :description, :craeted_by, :updated_by, :created_at, :updated_at
+json.url permission_url(permission, format: :json)
--- db/migrate/20170320181124_create_users.rb
+++ db/migrate/20170320181124_create_users.rb
@@ -1,10 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :name
-      t.string :email
-      t.string :password
+      t.string :username, null: false, index: true, comment: 'Username of the user.'
+      t.string :name, null: false, index: true, comment: 'Full name of the user.'
+      t.string :email, null: false, index: true, comment: 'Main e-mail of the user.'
+      t.string :password, null: false, comment: "Hashed version of the user's password."
+
+      t.integer :created_by, null: false, comment: 'Id of the user who created the record.'
+      t.integer :updated_by, null: false, comment: 'Id of the user who did the last change in the record.'
 
       t.timestamps
     end

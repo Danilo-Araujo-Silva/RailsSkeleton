558a6f43a2d7187f8d53b7838e2b7ce97830469b
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-22 10:05:53 -0300

    Including fake gem to create fake data for tests. Improving the users controller test.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,13 +2,16 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = User.new
-    @user.username = Faker::Name.unique.first_name.downcase
-    @user.name = Faker::Name.name
-    @user.email = Faker::Internet.unique.email
-    @user.password = Faker::Lorem.characters(10)
-    @user.created_by_id = 1
-    @user.updated_by_id = 1
+    @existent_user = users(:two)
+
+    @new_user = User.new
+    @new_user.id = Faker::Number.number(5)
+    @new_user.username = Faker::Name.unique.first_name.downcase
+    @new_user.name = Faker::Name.name
+    @new_user.email = Faker::Internet.unique.email
+    @new_user.password = Faker::Lorem.characters(10)
+    @new_user.created_by_id = 1
+    @new_user.updated_by_id = 1
   end
 
   test "should get index" do
@@ -23,30 +26,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, name: @user.name, password: @user.password, username: @user.username } }
+      post users_url, params: { user: { email: @new_user.email, name: @new_user.name, password: @new_user.password, username: @new_user.username } }
     end
 
     assert_redirected_to user_url(User.last)
   end
 
   test "should show user" do
-    get user_url(@user)
+    get user_url(@existent_user)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_user_url(@user)
+    get edit_user_url(@existent_user)
     assert_response :success
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name, password: @user.password, username: @user.username } }
-    assert_redirected_to user_url(@user)
+    patch user_url(@existent_user), params: { user: { email: @existent_user.email, name: @existent_user.name, password: @existent_user.password, username: @existent_user.username } }
+    assert_redirected_to user_url(@existent_user)
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete user_url(@user)
+      delete user_url(@existent_user)
     end
 
     assert_redirected_to users_url

41d22362a27b5d1da89be4b9afc4dec926158b35
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-24 19:24:24 -0300

    Refactorying routes and subsistems. Initiating.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,32 @@
 Rails.application.routes.draw do
+
+  root to: "public#landing"
+
+  namespace :administration do
+    get 'dashboard'
+  end
+
+  namespace :development do
+    get 'dashboard'
+  end
+
+  get 'about', to: 'public#about'
+  get 'careers', to: 'public#careers'
+  get 'help', to: 'public#help'
+  get 'landing', to: 'public#landing'
+  get 'privacy_policy', to: 'public#privacy_policy'
+  get 'sitemap', to: 'public#sitemap'
+  get 'terms_of_service', to: 'public#terms_of_service'
+  get 'error/not_found', to: 'public/error#not_found'
+
+  namespace :user do
+    get 'dashboard'
+  end
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users_controller/omniauth_callbacks" }
-
-  root to: "users#index"
-
-  resources :users_permissions
-  resources :permissions
-  resources :users
+  # resources :users_permissions
+  # resources :permissions
+  # resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
